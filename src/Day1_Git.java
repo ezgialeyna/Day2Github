82b806e9e7f6a6998a4c0eaca131f27ee62fdf85
Ezgi Demirci <aezgidemirci@gmail.com>
2022-08-18 18:10:11 +0300

    Ikinci Commit

--- src/Day1_Git.java
+++ src/Day1_Git.java
@@ -1,5 +1,7 @@
 public class Day1_Git {
     public static void main(String[] args) {
         System.out.println("Hello Git 1.deneme");
+        System.out.println("Hello Git 2.deneme");
+
     }
 }
